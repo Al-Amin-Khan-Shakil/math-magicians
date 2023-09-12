314000e7f061316e380f6c2bd0f08c728486b3ad
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-09-13 00:49:03 +0600

    Create components folder and calculator files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import CalculatorButtons from './components/CalculatorUI';
 
 function App() {
-  
+  return (
+    <div className="App">
+      <CalculatorButtons />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CalculatorUI.js
@@ -0,0 +1,9 @@
+import './CalculatorUI.css';
+
+function CalculatorButtons() {
+  return (
+    <div className="main-container">Hello World!</div>
+  );
+}
+
+export default CalculatorButtons;
