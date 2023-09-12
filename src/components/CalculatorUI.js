e0d5c0695b3a1a7a5ac29e1a18b7b48a0a8b9fdc
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-09-13 04:32:03 +0600

    Change design accroding to requirement

--- src/components/CalculatorUI.js
+++ src/components/CalculatorUI.js
@@ -9,22 +9,22 @@ function CalculatorButtons() {
         <button type="button" className="keypad">AC</button>
         <button type="button" className="keypad">+/-</button>
         <button type="button" className="keypad">%</button>
-        <button type="button" className="keypad">&#247;</button>
+        <button type="button" className="keypad last-col">&#247;</button>
         <button type="button" className="keypad">7</button>
         <button type="button" className="keypad">8</button>
         <button type="button" className="keypad">9</button>
-        <button type="button" className="keypad">x</button>
+        <button type="button" className="keypad last-col">x</button>
         <button type="button" className="keypad">4</button>
         <button type="button" className="keypad">5</button>
         <button type="button" className="keypad">6</button>
-        <button type="button" className="keypad">-</button>
+        <button type="button" className="keypad last-col">-</button>
         <button type="button" className="keypad">1</button>
         <button type="button" className="keypad">2</button>
         <button type="button" className="keypad">3</button>
-        <button type="button" className="keypad">+</button>
+        <button type="button" className="keypad last-col">+</button>
         <button type="button" className="keypad num-zero">0</button>
         <button type="button" className="keypad">.</button>
-        <button type="button" className="keypad">=</button>
+        <button type="button" className="keypad last-col">=</button>
       </div>
     </div>
   );
