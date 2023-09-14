53832059afa6b5bef867540636b02161c639bacf
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-09-14 14:33:30 +0600

    Add value to buttons

--- src/components/CalculatorUI.js
+++ src/components/CalculatorUI.js
@@ -1,30 +1,69 @@
 import './CalculatorUI.css';
-import Output from './Output';
 
 function CalculatorButtons() {
   return (
     <div className="main-container">
       <div className="cal-container">
-        <Output />
-        <button type="button" className="keypad">AC</button>
-        <button type="button" className="keypad">+/-</button>
-        <button type="button" className="keypad">%</button>
-        <button type="button" className="keypad last-col">&#247;</button>
-        <button type="button" className="keypad">7</button>
-        <button type="button" className="keypad">8</button>
-        <button type="button" className="keypad">9</button>
-        <button type="button" className="keypad last-col">x</button>
-        <button type="button" className="keypad">4</button>
-        <button type="button" className="keypad">5</button>
-        <button type="button" className="keypad">6</button>
-        <button type="button" className="keypad last-col">-</button>
-        <button type="button" className="keypad">1</button>
-        <button type="button" className="keypad">2</button>
-        <button type="button" className="keypad">3</button>
-        <button type="button" className="keypad last-col">+</button>
-        <button type="button" className="keypad num-zero">0</button>
-        <button type="button" className="keypad">.</button>
-        <button type="button" className="keypad last-col">=</button>
+        <div className="output" dir="rtl">
+          0
+        </div>
+        <button type="button" className="keypad" value="AC">
+          AC
+        </button>
+        <button type="button" className="keypad" value="+/-">
+          +/-
+        </button>
+        <button type="button" className="keypad" value="%">
+          %
+        </button>
+        <button type="button" className="keypad last-col" value="&#247;">
+          &#247;
+        </button>
+        <button type="button" className="keypad" value="7">
+          7
+        </button>
+        <button type="button" className="keypad" value="8">
+          8
+        </button>
+        <button type="button" className="keypad" value="9">
+          9
+        </button>
+        <button type="button" className="keypad last-col" value="x">
+          x
+        </button>
+        <button type="button" className="keypad" value="4">
+          4
+        </button>
+        <button type="button" className="keypad" value="5">
+          5
+        </button>
+        <button type="button" className="keypad" value="6">
+          6
+        </button>
+        <button type="button" className="keypad last-col" value="-">
+          -
+        </button>
+        <button type="button" className="keypad" value="1">
+          1
+        </button>
+        <button type="button" className="keypad" value="2">
+          2
+        </button>
+        <button type="button" className="keypad" value="3">
+          3
+        </button>
+        <button type="button" className="keypad last-col" value="+">
+          +
+        </button>
+        <button type="button" className="keypad num-zero" value="0">
+          0
+        </button>
+        <button type="button" className="keypad" value=".">
+          .
+        </button>
+        <button type="button" className="keypad last-col" value="=">
+          =
+        </button>
       </div>
     </div>
   );
