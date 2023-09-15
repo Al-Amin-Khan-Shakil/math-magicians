0400c1df61d0b3e60abfbd16eb83919afc3f5069
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-09-16 01:52:03 +0600

    Fetch data from api

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,5 +1,47 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-const Quotes = () => <p>The quotes will be displayed here</p>;
+const url = "https://api.api-ninjas.com/v1/quotes?category=computers";
+const apiKey = "RCx4GyRkAnqwLwS2DBq/cg==6hZ3KyhXY6HyLkdm";
+
+const Quotes = () => {
+  const [quotes, setQuotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const getData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "X-Api-Key": apiKey,
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setQuotes(data[0]);
+      setIsLoading(false);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="quote-container">
+      <blockquote className="quote">
+        <q className="qtag">{quotes.quote}</q>
+        <cite className="author">{quotes.author}</cite>
+      </blockquote>
+    </div>
+  );
+};
 
 export default Quotes;
