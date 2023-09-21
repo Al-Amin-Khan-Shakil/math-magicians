0b52d1f21b24167604d776c5e768c0fdc6552432
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-09-21 23:10:51 +0600

    Update App snapshot and test calculate.js

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -2,16 +2,13 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 
-// eslint-disable-next-line arrow-body-style, no-unused-vars
-const mockApp = () => {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-};
+const mockApp = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 it('Test App component', () => {
-  const tree = renderer.create(<mockApp />).toJSON();
+  const tree = renderer.create(mockApp()).toJSON();
   expect(tree).toMatchSnapshot();
 });
